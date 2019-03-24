4a73c4f259f5f09ff99717be1d321d0eb6057ac3
Mozhgan Ghasemzadeh <45165104+mozhgan-ghasemzadeh@users.noreply.github.com>
2019-03-24 21:35:58 +0000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,73 @@
+function loadPage(){
+	var txt =location.href.split("#"),li;
+
+    if(txt.length==1)
+        li = "about";
+    else 
+        li = txt[txt.length-1];
+    
+    var ele = document.querySelector("ul").children;
+    var ele = document.querySelector(' a[ href = "#'+li+'" ]');
+    ele.parentNode.style.backgroundColor = "rgb(140,130,121,0.3)";
+}
+function ChangeTab(e){
+	
+	var ul = document.querySelector(".navWrapper>ul").children;
+	for (var i = 0; i < ul.length ; i++) {
+		ul[i].style.backgroundColor = "rgb(0,61,76)";
+		ul[i].style.color = "white";
+	
+	}
+	
+	ul[e].style.backgroundColor = "rgb(140,130,121,0.3)";
+	ul[e].style.color = "white";
+	n=e;
+	}
+
+function WriteEducation(){
+	var element=document.querySelector("article#education>div"),txt="";
+	// var arr = { "one": 1, "two": [2,3], "three": 3 }; 
+	// element.innerHTML=arr["two"]+arr["one"];
+	// alert(element.nodeName);
+	
+	for (var i = 0; i < edu.length; i++) {
+		txt+=("<h1>" + edu[i]["section"] + "</h1>");
+		txt+="<br> <ul>";
+		for (var j = 0; j < edu[i]["li"].length; j++) {
+			txt+=("<li>"+edu[i]["li"][j]+"</li>");
+		}
+		txt += "</ul><br>";
+	}
+
+	// element.innerHTML=edu[0]["section"]+"<br>"+edu[0]["li"][1][1] + "<br>"+edu[1]["li"][0];
+	element.innerHTML = txt;
+}
+function WritePublication(){
+	
+	var element=document.querySelector("article#publication>div"),
+	txt="<h1>Publication</h1>",url;
+	for (var i = 0; i < pub.length; i++) {
+		txt+=("<h1 style = ' margin-top:40px;'>"+pub[i]["section"]+"</h1>");
+		txt+="<ul>";
+		for (var j = 0; j < pub[i]["li"].length; j++) {
+			txt+=("<li><p>"+pub[i]["li"][j][0]+"</p><p class = 'bold'>"+pub[i]["li"][j][1]+"</p><p class = 'conf'><i>"+pub[i]["li"][j][2]+"</i></p>")
+			if(pub[i]["li"][j][3]==""){
+				txt+="</li>";
+			}
+			else{
+				// url = pub[i]["li"][j][3].split(":");
+				// txt+=("<h5><a class='linkDoi' href=https://doi.org/"+url[1]+">"+pub[i]["li"][j][3]+"</a></h5></li>");
+				txt+=("<h5><a class='linkDoi' href=https://doi.org/"+pub[i]["li"][j][3]+"><span>DOI</span><span>"+pub[i]["li"][j][3]+"</span></a></h5></li>");
+			}
+			
+			
+			// txt+=(pub[i]["li"][j]+"</li>");
+		}
+		
+		txt += "</ul>";
+	}
+	element.innerHTML = txt;
+}
+loadPage();
+WriteEducation();
+WritePublication();
---
+++ data.js
@@ -0,0 +1,39 @@
+var edu = [
+	{"section":"Education", 
+	"li" : ["Dec 2018 - Present : Brunel","Apr 2017 - Nov 2018  : Postdoctoral in project ANTAREX, FEUP ","Oct 2015 - Feb 2017 : Postdoctoral at IC4, Dublin City University (DCU)","Feb 2011 - Aug 2015 : PhD in Computer Science, University of Porto"]}
+	,{"section":"Visiting Scholar", 
+	"li" : ["IN2P3 Computing Center, Lyon (France), June 2012, funded by COST Action on Open European Network for High Performance Computing on Complex Environments (COST Action IC0805 ComplexHPC)","Distributed and Parallel Systems Group (DPS), University of Innsbruck, Austria, October 2014, as part of the Short Term Scientific Mission (STSM) supported by the Network for Sustainable Ultrascale Computing (NESUS) COST Action"]},
+	{ "section" :"Program Committee Member",
+	"li":["The 15th International Workshop on Algorithms, Models and Tools for Parallel Computing on Heterogeneous Platforms (HeteroPar'17)","The 8th IEEE International Conference on Cloud Computing Technology and Science (CloudCom'16)","The 7th IEEE International Conference on Cloud Computing Technology and Science (CloudCom'15)","The 6th International Conference On Cloud Computing : Services and Architecture (CLOUD 2017)","Doctoral Symposium on Informatics Engineering (DSIE'2012)"]
+	}
+	];
+
+// ["author name","publication name","publication conference","DOI"]
+var pub = [
+	{"section":"Journal Publications",
+	"li":[
+	["Hamid Arabnejad, Jorge G. Barbosa.","Multi-QoS constrained and Profit-aware scheduling approach for concurrent workflows on heterogeneous systems","Future Generation Computer Systems (FGCS), vol. 68, pp. 211&ndash;221, 2017.","10.1016/j.future.2016.10.003 "],
+	["Hamid Arabnejad, Jorge G. Barbosa., Radu Prodan ","Low-Time Complexity Budget-Deadline Constrained Workflow Scheduling on heterogeneous resources","Future Generation Computer Systems (FGCS), vol. 55, pp. 29&ndash;40, 2016","10.1016/j.future.2015.07.021"],
+	["Hamid Arabnejad, Jorge G. Barbosa.","List scheduling algorithm for heterogeneous systems by an optimistic cost table","IEEE Transactions on Parallel and Distributed Systems (TPDS), vol. 25, no. 3, pp. 682&ndash;694, 2014.","10.1109/TPDS.2013.57 "],
+	["Hamid Arabnejad, Jorge G. Barbosa.","A Budget Constrained Scheduling Algorithm for Workflow Applications","Journal of Grid Computing, vol. 12, no. 4, pp. 665&ndash;679, 2014","10.1007/s10723-014-9294-7 "],
+	["Hamid Arabnejad, Jorge G. Barbosa.","Maximizing the completion rate of concurrent scientific applications under time and budget constraints","Journal of Computational Science, In Press, Accepted Manuscript, 2016.","10.1016/j.jocs.2016.10.013"]
+	// ["","","",""]
+	]},
+	{"section":"Book Chapters",
+	"li":[
+	["Hamid Arabnejad, Jorge G. Barbosa, and Fre&#769;de&#769;ric Suter. ","Fair Resource Sharing for Dynamic Scheduling of Workflows on Heterogeneous Systems ","High-Performance Computing on Complex Environments,ch. 9, pp. 145&ndash;167, 2014.","edu10.1002/9781118711897.ch9"],
+	["Jorge G. Barbosa, Christos Michalakelis, Hamid Arabnejad. ","Resource Sharing for Scientific Workflows on Computational Grids","Grid Computing: Techniques and Future Prospects, pp. 45&ndash;70, 2015. ",""]]},
+	{"section":"Conference Publications",
+	"li":[
+	["Hamid Arabnejad, Claus Pahl, Areeg Samir and Giovani Estrada.","A Fuzzy Load Balancer for Adaptive Fault Tolerance Management in Cloud Platforms ","The 6th European Conference on Service-Oriented and Cloud Computing (ESOCC'2017).","10.1007/978-3-319-67262-5_9"],//change conf name
+	["Hamid Arabnejad, Claus Pahl, Pooyan Jamshidi, Giovani Estrada.","A Comparison of Reinforcement Learning Techniques for Fuzzy Cloud Auto-Scaling <i class='fa fa-bullhorn' id='icon'></i><a id='nominated' target='_blanck' href='https://easychair.org/smart-program/CCGRID2017/2017-05-15.html#session:14584' >nominated for the best paper award </a>","The 17th IEEE/ACM International Symposium on Cluster, Cloud and Grid Computing (CCGrid'2017), pp. 64-73, 2017."," 10.1109/CCGRID.2017.15 "],
+	["Mozhgan Ghasemzadeh, Hamid Arabnejad, Jorge Barbosa.","Deadline-Budget constrained Scheduling Algorithm for Scientific Workflows in a Cloud Environment ","The 20th International Conference on Principles of Distributed Systems (OPODIS 2016), vol. 70, p. 19, 2017.","10.4230/LIPIcs.OPODIS.2016.19 "],
+	["Hamid Arabnejad, Pooyan Jamshidi, Giovani Estrada, Nabil El Ioini, Claus Pahl. ","An Auto-Scaling Cloud Controller Using Fuzzy Q-Learning - Implementation in OpenStack","The 5th European Conference on Service-Oriented and Cloud Computing (ESOCC'2016), pp. 152&ndash;167, 2016."," 10.1007/978-3-319-44482-6_10 "],
+	["Pooyan Jamshidi, Amir Sharifloo, Claus Pahl, Hamid Arabnejad, Andreas Metzger, Giovani Estrada.","Fuzzy Self-Learning Controllers for Elasticity Management in Dynamic Cloud Architectures ","The 12th International ACM SIGSOFT Conference on Quality of Software Architectures (QoSA'2016), pp. 70&ndash;79, 2016"," 10.1109/QoSA.2016.13 "],
+	["Hamid Arabnejad, Jorge G. Barbosa.","Multi-workflow QoS-Constrained Scheduling for Utility Computing ","The 18th IEEE International Conference on Computational Science and Engineering (CSE), pp. 137&ndash;144, 2015.","10.1109/CSE.2015.29 "],
+	["Hamid Arabnejad, Jorge G. Barbosa. ","Budget Constrained Scheduling Strategies for On-line Workflow Applications ","The 14th International Conference on Computational Science and Its Applications (ICCSA), pp. 532&ndash;545, 2014. ","10.1007/978-3-319-09153-2_40"],
+	["Hamid Arabnejad, Jorge G. Barbosa. ","Fairness resource sharing for dynamic workflow scheduling on heterogeneous systems ","The 10th IEEE International Symposium on Parallel and Distributed Processing with Applications (ISPA), pp. 633&ndash;639, 2012.","10.1109/ISPA.2012.94 "],
+	["Hamid Arabnejad, Jorge G. Barbosa. ","Performance evaluation of list based scheduling on heterogeneous systems ","Euro-Par 2011 : Parallel Processing Workshops, pp. 440&ndash;449, 2011.","10.1007/978-3-642-29737-3_49 "]
+	]},
+	{"section":"Other Publications",//add link
+	"li":[["Hamid Arabnejad ","List Based Task Scheduling Algorithms on Heterogeneous Systems - An overview ","Doctoral Symposium in Informatics Engineering (DSIE'12), pp. 93&ndash;102, 2013. ",""]]}];
